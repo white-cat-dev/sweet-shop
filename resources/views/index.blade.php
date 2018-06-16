1027e7a11794bd1af5c4b6383b5ec83f3c870301
white-cat-dev <alexandra.19.98.study@gmail.com>
2018-06-17 02:40:16 +0800

    Update index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,24 +3,22 @@
 @section('content')
 <div class="container">
 	<div class="main">
+		<h1>Кабинет кондитера</h1>
 		<div class="main-block light-block">
-			Просмотр, редактирование и добавление новых тортов
+			<i class="fas fa-check"></i> Редактирование каталога тортов: просмотр, обновление и добавление новых
 
 		</div>
 
 		<div class="main-block dark-block">
-			Контроль количества ингредиентов и добавление новых
-
+			<i class="fas fa-check"></i> Контроль количества ингредиентов, удаление и добавление новых
 		</div>
 
 		<div class="main-block light-block">
-			Обработка новых заказов
-
+			<i class="fas fa-check"></i> Обработка новых заказов: просмотр, изменение статуса и удаление
 		</div>
 
 		<div class="main-block dark-block">
-			Новые комментарии
-
+			<i class="fas fa-check"></i> Просмотр новых отзывов клиентов и удаление непонравившихся
 		</div>
 	</div>
 </div>
